f895430946201096e243f7e0887d75feca5199fc
unknown <frederico.bender99@gmail.com>
2020-04-20 19:58:06 -0300

    ...

--- FARC - Sugeno/FARC - Sugeno/script1.sh
+++ FARC - Sugeno/FARC - Sugeno/script1.sh
@@ -1,6 +1,6 @@
-for a in magic;do
-    for b in {5,50};do
-        for c in {1,2,3,4,5};
+for a in {magic,satimage,page-blocks,ring,penbased,twonorm};
+    do for b in {5,50};
+        do for c in {1,2,3,4,5};
             do java -cp build/classes FARCHD.Main param.txt $a $b $c 5;           
         done;
     done;
--- FARC - Sugeno/FARC - Sugeno/script2.sh
+++
@@ -1,7 +0,0 @@
-for a in {satimage,page-blocks};do
-    for b in {5,50};do
-        for c in {1,2,3,4,5};
-            do java -cp build/classes FARCHD.Main param.txt $a $b $c 5;           
-        done;
-    done;
-done;
