a79cc9a4599dcb386ea3fd36c2f0dbf4b3faded1
Frederico Bender Tiggemann <frederico.bender99@gmail.com>
2021-07-02 12:48:21 -0300

    corrections in viewDatasets.py

--- ComplexityMeasures/ComplexityMeasures/data/Shape Datasets/viewDatasets.py
+++ ComplexityMeasures/ComplexityMeasures/data/Shape Datasets/viewDatasets.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 datasets=["aggregation","compound","pathbased","spiral","D31","R15","jain","flame"]
-rows , columns = 4 , 2
-plt.figure(figsize=(50,25))
+rows , columns = 3 , 3
+plt.figure(figsize=(40,40))
 
 for row in range(rows):
     for col in range(columns):
@@ -16,7 +16,7 @@ for row in range(rows):
         for name, group in data.groupby("species"):
             plt.plot(group["x"], group["y"], marker="o", linestyle="", label=name)
         plt.grid(True)
-        plt.title(datasets[row*columns + col])
+        plt.title(datasets[row*columns + col],fontsize=45)
         plt.legend(loc="center right",bbox_to_anchor=(1.12, 0.5))
 
 plt.savefig("datasets.pdf",bbox_inches='tight')
